472df02aebe8876671bd1e2a0851c796ea4ff2a5
pengjie <pengjie0668@aliyun.com>
2021-01-06 14:19:18 +0800

    添加native 调用java方法

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,25 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
+#define LOGD(FORMAT, ...) __android_log_print(ANDROID_LOG_DEBUG,"pengjie0668",FORMAT,##__VA_ARGS__);
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_pengjie0668_demo_myapplication_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "你好 from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_pengjie0668_demo_myapplication_MainActivity_callJavaMethodTest(JNIEnv *env, jobject thiz,jstring text) {
+    //通过传进来的对象找到该类
+    jclass javaClass = env->GetObjectClass(thiz);
+    if (javaClass == 0) {
+        return;
+    }
+    //获取要回调的方法ID,回调java方法
+    jmethodID javaMethodId = env->GetMethodID( javaClass, "callJavaMethod", "(Ljava/lang/String;)V");
+    env->CallVoidMethod( thiz, javaMethodId ,text);
 }
--- app/src/main/java/com/pengjie0668/demo/myapplication/MainActivity.java
+++ app/src/main/java/com/pengjie0668/demo/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.pengjie0668.demo.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +22,24 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callJavaMethodTest("您回调了java层方法");
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+
+    //java层调用native层方法
     public native String stringFromJNI();
+
+    //native层回调java层方法
+    public void callJavaMethod(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+
+    //java层调用native层方法
+    public native void callJavaMethodTest(String text);
+
 }
